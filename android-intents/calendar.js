a9b593a3f853b9901bf8fba68e3ca82dd661e572
evasua <evasua@gmail.com>
2017-01-26 15:26:45 +0300

    Update calendar.js

--- android-intents/calendar.js
+++ android-intents/calendar.js
@@ -1,4 +1,11 @@
-function createEvent(description) 
+function createEvent(title, description, begin , end, location) 
 {
-  message('Test OK2: ' + description)
+  i = intent("android.intent.action.INSERT");       
+  i.data("content://com.android.calendar/events");  
+  i.extra("title", title);
+  i.extra("description", description);                  
+  i.extraLong("beginTime", begin.getTime());                                       
+  i.extraLong("endTime", end.getTime());                      
+  e.extra("eventLocation", location);
+  i.send();                                       
 }
