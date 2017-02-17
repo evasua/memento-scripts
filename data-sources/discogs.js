73359f9521a5988b421e9ccbb651cc840511ec24
evasua <evasua@gmail.com>
2017-02-17 20:48:14 +0300

    Update discogs.js

--- data-sources/discogs.js
+++ data-sources/discogs.js
@@ -1,12 +1,17 @@
-function Discogs (apiKey , apiSecret) {
+function Discogs (apiKey , apiSecret, type) {
     this.apiKey = apiKey;
     this.apiSecret = apiSecret;
+    this.type = type;
 }
 
-Discogs.prototype.search = function(type , query) {
-  var result = http().get("https://api.discogs.com/database/search?q=" + query + "&key=" + this.apiKey + "&secret=" + this.apiSecret + "&type=" + type);
+Discogs.prototype.search = function(query) {
+  var result = http().get("https://api.discogs.com/database/search?q=" + query + "&key=" + this.apiKey + "&secret=" + this.apiSecret + "&type=" + this.type);
   var json = JSON.parse(result.body);
   return json.results;  
 }
 
+Discogs.prototype.extra = function(id) {
+     var result = http().get("https://api.discogs.com/" + this.type + "/" + id);
+     return JSON.parse(result.body); 
+}
 
