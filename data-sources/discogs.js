82f60b6f643fabdae4fbf38d92c784f1d8c2aeba
evasua <evasua@gmail.com>
2017-02-17 20:49:53 +0300

    Update discogs.js

--- data-sources/discogs.js
+++ data-sources/discogs.js
@@ -11,6 +11,7 @@ Discogs.prototype.search = function(query) {
 }
 
 Discogs.prototype.extra = function(id) {
+    message("selected id :" + id);
      var result = http().get("https://api.discogs.com/" + this.type + "/" + id);
      return JSON.parse(result.body); 
 }
